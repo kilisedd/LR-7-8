ed1c1eee453f7207f0967c28e93ee54b063a836a
kilisedd <117628693+kilisedd@users.noreply.github.com>
2022-11-11 13:05:09 +0300

    Add files via upload

---
+++ zad5.pas
@@ -0,0 +1,10 @@
+﻿program z5;
+var a:string;
+z,x:integer;
+begin
+  writeln('введите строку: ');
+  read(a);
+  z:=length(a);
+  for x:=1 to z do
+    if a[x]=copy(a,z,x) then writeln('позиции повтора: ',x);
+end.
